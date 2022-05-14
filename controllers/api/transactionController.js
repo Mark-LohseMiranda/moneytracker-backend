e68790fcacece77e3638c08b055e47ee9303180e
mark.lohsemiranda <mark.lohsemiranda@gmail.com>
2022-05-14 09:56:08 -0700

    added getone and delete

--- controllers/api/transactionController.js
+++ controllers/api/transactionController.js
@@ -3,13 +3,41 @@ const { authMiddleware } = require("../../utils/auth");
 const router = require("express").Router();
 
 router.get("/getall", authMiddleware, (req, res) => {
-  Transaction.findAll({ where: { UserId: req.user.id }, order: [['date','DESC']] })
+  Transaction.findAll({
+    where: { UserId: req.user.id },
+    order: [["date", "DESC"]],
+  })
     .then((data) => {
       res.status(200).json(data);
     })
     .catch((err) => {
-      res.status(500).json({ err });
+      res.status(500).json(err);
     });
+});
+
+router.get("/getone/:id", authMiddleware, (req, res) => {
+  try {
+    Transaction.findOne({
+      where: {
+        id: req.params.id,
+        UserId: req.user.id,
+      },
+    })
+      .then((data) => {
+        if (!data) {
+          res
+            .status(403)
+            .json({ message: "Not your transaction or transaction not found" });
+          return;
+        }
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/add", authMiddleware, (req, res) => {
@@ -19,23 +47,53 @@ router.post("/add", authMiddleware, (req, res) => {
     date: req.body.date,
     cleared: req.body.cleared,
     UserId: req.user.id,
-  }).then((newTrans)=>{
-      res.status(200).json(newTrans);
-  }).catch((err)=>{
-      res.status(500).json({err});
   })
+    .then((newTrans) => {
+      res.status(200).json(newTrans);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-router.put("/:id",authMiddleware,(req,res)=>{
-  Transaction.update(req.body,{
-    where: {
-      id: req.params.id
+router.put("/:id", authMiddleware, async (req, res) => {
+  try {
+    const Transupdate = await Transaction.update(req.body, {
+      where: {
+        id: req.params.id,
+        UserId: req.user.id,
+      },
+    });
+    if (!Transupdate[0]) {
+      res
+        .status(403)
+        .json({ message: "Not your transaction or transaction not found" });
+      return;
     }
-  }).then((updated)=>{
-    res.status(200).json(updated)
-  }).catch((err)=>{
-    res.status(500).json({err})
-  })
-})
+    res.status(200).json(Transupdate);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", authMiddleware, async (req, res) => {
+  try {
+    const Transdelete = await Transaction.destroy({
+      where: {
+        id: req.params.id,
+        UserId: req.user.id,
+      },
+    });
+    if (!Transdelete) {
+      res
+        .status(403)
+        .json({ message: "Not your transaction or transaction not found" });
+      return;
+    }
+    res.status(200).json(Transdelete);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
